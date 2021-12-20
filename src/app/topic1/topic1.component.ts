64aa40f830e6b6fa48da71ab3c89522fbd52788f
Vivekanand P V <vivekanandpv@gmail.com>
2021-12-20 12:46:35 +0530

    comment added

--- src/app/topic1/topic1.component.ts
+++ src/app/topic1/topic1.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-topic1',
   templateUrl: './topic1.component.html',
-  styleUrls: ['./topic1.component.css']
+  styleUrls: ['./topic1.component.css'],
 })
 export class Topic1Component implements OnInit {
   year;
   city;
 
-  constructor(private ar: ActivatedRoute) {}
+  constructor(private ar: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     this.year = this.ar.snapshot.paramMap.get('year');
     this.city = this.ar.snapshot.paramMap.get('city');
+
+    //  Professionally this is preferred (internal navigation with routerLink)
+    // this.ar.paramMap.subscribe((rm) => {
+    //   this.year = rm.get('year');
+    //   this.city = rm.get('city');
+    // });
   }
 }
